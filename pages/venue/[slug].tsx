443c0155785c2d1ec32d0849ab7fcc2b39129493
Roy <toledoroy@gmail.com>
2023-10-08 10:01:07 +0700

    Auto Dates & Custom Design

--- pages/venue/[slug].tsx
+++ pages/venue/[slug].tsx
@@ -2,19 +2,30 @@ import { useRouter } from "next/router";
 import Schedule from "../../components/Schedule";
 import { Box, Container, Tooltip, Typography } from "@mui/material";
 import ImageIcon from '@mui/icons-material/Image';
+import { timeHelper } from "../../helpers/timeHelper";
 
 /**
  * Single Venue Page
  */
-export default function SingleVenuePge(): JSX.Element {
+export default function SingleVenuePge(
+    {weekNum = timeHelper.currentWeek()} //new Date('01/05/2023')
+    ): JSX.Element {
     const router = useRouter();
     const { slug } = router.query;
     
+    // const weekNum = timeHelper.currentWeek();
+    console.warn("weekNum:", {weekNum,
+        sunday: timeHelper.getDayOfWeek(7, weekNum), //Last Sunday
+        monday: timeHelper.getDayOfWeek("monday", weekNum),
+        tuesday: timeHelper.getDayOfWeek(0, weekNum), //First Sunday (Before)
+        saturday: timeHelper.getDayOfWeek("saturday", weekNum),
+    });
+
     //Demo Data
     const data: {[key: string]: any} = {
         yogahouse:{
             name: "The Yoga House",
-            header:'Weekly Schedule 00-00/00',
+            header:`Weekly Schedule ${timeHelper.getDayOfWeek("monday", weekNum).format('DD')}-${timeHelper.getDayOfWeek(6, weekNum).format('DD/MM')}`,
             footer:'',
             logo: '/images/logo_yogahouse.png'
         }
@@ -29,11 +40,13 @@ export default function SingleVenuePge(): JSX.Element {
       >
         <Typography variant="h1" sx={{textTransform:'capitalize'}}>{venueData?.name || ''}</Typography>
         <Toolbar />
-        <div className="schedule_header">
-            {!!venueData?.logo && <img className="venue_logo" src={venueData.logo} />}
-            {venueData?.header}
+        <div id="schedule_display" className={`schedule_${slug}`}>
+            <div className="schedule_header">
+                {!!venueData?.logo && <img className="venue_logo" src={venueData.logo} />}
+                {venueData?.header}
+            </div>
+            <Schedule />
         </div>
-        <Schedule />
         <div className="schedule_footer">{venueData?.footer}</div>
     </Container>
     );
@@ -43,10 +56,11 @@ export default function SingleVenuePge(): JSX.Element {
  * Actions Toolbar
  */
 const Toolbar = (): JSX.Element => (
-<Box id="toolbar" sx={{backgroundColor:'primary'}}>
-    <LangSelect />
-    <ImageExport />
-</Box>)
+    <Box id="toolbar" sx={{backgroundColor:'primary'}}>
+        <LangSelect />
+        <span style={{margin: '0 20px'}}>Week {timeHelper.currentWeek()}</span>
+        <ImageExport />
+    </Box>)
 
 const LangSelect = (): JSX.Element => (<>
     <Tooltip title="English">
